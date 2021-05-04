76ec4ff1f5c24ae9a40ba363b68423549af5b0a0
natgoodman <natg@shore.net>
2021-05-04 07:54:38 -0700

    Moved 'assign_global' to new R/assign.R

--- R/param.R
+++ R/param.R
@@ -60,22 +60,24 @@ param=function(...,list=character()) {
 ## do it this way until 'list' arg of 'param' function can handle params with new values 
 ## assign(paste(sep='.','extra',what),fun,envir=param.env);
 
-## if called without args, copies parent's local variables to global
-##   used in repwr to simplify init
-## NG 21-03-02: if called with args, copies parent's values for args to global
-assign_global=function(...,list=character()) {
-  dots=match.call(expand.dots=FALSE)$...
-  parent.env=parent.frame(n=1);
-  if (length(dots) &&
-      !all(vapply(dots,function(x) is.atomic(x)||is.symbol(x)||is.character(x),
-                  NA,USE.NAMES=FALSE))) 
-    stop("... must contain atomic data like names or character strings");
-  names=vapply(dots,as.character,"");
-  if (length(names)==0L) names=character();
-  names=c(list,names);
-  if (!length(names)) names=ls(envir=parent.env);
-  sapply(names,function(name) assign(name,get(name,envir=parent.env),envir=.GlobalEnv));
-}
+## NG 21-05-02: moved to assign.R
+## ## if called without args, copies parent's local variables to global
+## ##   used in repwr to simplify init
+## ## NG 21-03-02: if called with args, copies parent's values for args to global
+## assign_global=function(...,list=character()) {
+##   dots=match.call(expand.dots=FALSE)$...
+##   parent.env=parent.frame(n=1);
+##   if (length(dots) &&
+##       !all(vapply(dots,function(x) is.atomic(x)||is.symbol(x)||is.character(x),
+##                   NA,USE.NAMES=FALSE))) 
+##     stop("... must contain atomic data like names or character strings");
+##   names=vapply(dots,as.character,"");
+##   if (length(names)==0L) names=character();
+##   names=c(list,names);
+##   if (!length(names)) names=ls(envir=parent.env);
+##   sapply(names,function(name) assign(name,get(name,envir=parent.env),envir=.GlobalEnv));
+## }
+
 ## copy local variables to new or existing param environment - to simplify init
 init_param=function() {
   param.env=new.env(parent=emptyenv());
