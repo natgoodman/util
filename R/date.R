90f319f4a6708a44ea5c2ecbc1348ece02cd3d37
natgoodman <natg@shore.net>
2021-09-20 11:18:53 -0700

    Move 'mon-day' function from covid/R/label.R. Should have been done long ago Add support for 'last' day of month Rename 'Weekdays' to 'WEEKDAYS' (all caps) for stylistic consistency

--- R/date.R
+++ R/date.R
@@ -6,7 +6,7 @@
 ##          from misig/util.R created 19-01-01
 ##          from repwr/R/util.R created 18-05-03
 ##
-## Copyright (C) 2020 Nat Goodman.
+## Copyright (C) 2020-2021 Nat Goodman.
 ## 
 ## Date functions I use in covid project
 ##
@@ -15,6 +15,12 @@
 ##
 #################################################################################
 ## ---- Date functions ----
+
+## constants we use
+## names of days and months. number of days in months
+WEEKDAYS=cq(Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday);
+MONTHS=cq(January,February,March,April,May,June,July,August,September,October,November,December);
+MONTHDAYS=setNames(c(31,28, 31L,30,31,30,31,31,30,31,30,31),MONTHS);
 
 ## test if x is Date
 is_date=function(x) is_class(x,'Date')
@@ -46,9 +52,30 @@ as_date=function(x) {
 ## convert date to version strings we use, eg, 20-05-13
 as_version=function(date) strftime(date,format='%y-%m-%d')
 ## day-of-week manipulation we use
-Weekdays=cq(Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday);
-dayofweek=Vectorize(function(day) which(Weekdays==day))
+dayofweek=Vectorize(function(day) which(WEEKDAYS==day))
 match_day=function(day) match.arg(ucfirst(day),Weekdays)
 inc_day=function(day,i=0) Weekdays[((dayofweek(day)+i-1)%%7)+1]               
 ## get date of Sunday for week containing date
 sunday_week=function(date) date-dayofweek(weekdays(date))+1
+
+## adapted from www.r-bloggers.com/dates-in-r-and-the-first-day-of-the-month. Thx!
+## NG 21-09-19: code moved here from covid/R/label.R
+##              extended to get last day of month
+mon_day=function(date,day) {
+  date=as_date(date);                   # in case comes to us numeric
+  repv(date,day);                       # extend args to same length
+  ## as.Date(paste(sep='-',unique(format(date,format="%Y-%m")),day))
+  if (any(day=='last')) {
+    month=months(date);
+    year=format(date,format="%Y");
+    leap=is_leap(year);
+  }
+  day=ifelse(day!='last',day,ifelse(month!='February'|!leap,MONTHDAYS[month],29));
+  as_date(paste(sep='-',unique(format(date,format="%Y-%m")),day))
+}
+## adapted from lubridate. Thx!
+is_leap=function(year) {
+  year=as.numeric(year);
+  (year%%4==0)&((year%%100!=0)|(year%%400==0));
+}
+
