4db126c3b9c53024856a232fd56cc3a2df56c91b
natgoodman <natg@shore.net>
2021-09-22 11:43:27 -0700

    Add support for abbreviated (3-letter) day and month names Generalize 'mon_day' and related to handle dates as well as day names Add functions for months similar to day functions Add 'inc_day' and 'inc_month' for date addition

--- R/date.R
+++ R/date.R
@@ -18,9 +18,17 @@
 
 ## constants we use
 ## names of days and months. number of days in months
-WEEKDAYS=cq(Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday);
-MONTHS=cq(January,February,March,April,May,June,July,August,September,October,November,December);
-MONTHDAYS=setNames(c(31,28, 31L,30,31,30,31,31,30,31,30,31),MONTHS);
+init_date=function() {
+  WEEKDAYS<<-cq(Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday);
+  WDAYS<<-substr(WEEKDAYS,1,3)
+  MONTHS<<-cq(January,February,March,April,May,June,
+              July,August,September,October,November,December);
+  MONS<<-substr(MONTHS,1,3);
+  MONTHDAYS<<-setNames(c(31,28,31,30,31,30,31,31,30,31,30,31),MONTHS);
+  MONDAYS<<-setNames(MONTHDAYS,MONS);
+  INIT.DATE<<-TRUE;
+}
+if (!exists('INIT.DATE')||!INIT.DATE) init_date();
 
 ## test if x is Date
 is_date=function(x) is_class(x,'Date')
@@ -52,12 +60,39 @@ as_date=function(x) {
 ## convert date to version strings we use, eg, 20-05-13
 as_version=function(date) strftime(date,format='%y-%m-%d')
 ## day-of-week manipulation we use
-dayofweek=Vectorize(function(day) which(WEEKDAYS==day))
-match_day=function(day) match.arg(ucfirst(day),Weekdays)
-inc_day=function(day,i=0) Weekdays[((dayofweek(day)+i-1)%%7)+1]               
+## day is date or day string (eg, Mon or Monday)
+dayofweek=Vectorize(function(day) {
+  if (is_date(day)) day=weekdays(day) else day=ucfirst(day);
+  if (nchar(day)>3) which(WEEKDAYS==day) else which(WDAYS==day);
+})
+match_day=Vectorize(function(day) {
+  if (is_date(day)) day=weekdays(day) else day=ucfirst(day);
+  if (nchar(day)>3) match.arg(day,WEEKDAYS) else match.arg(day,WDAYS);
+})
+## TODO: figure out how to vectorize: Vectorize turns dates into ints!
+inc_day=function(day,i=1) {
+  if (is_date(day)) return(day+i);
+  nday=((dayofweek(day)-1+i)%%7)+1;
+  if (nchar(day)>3) WEEKDAYS[nday] else WDAYS[nday];
+}
+## month-of-year manipulation we use
+## month is date or month string (eg, Jan or January)
+monthofyear=monofyear=Vectorize(function(month) {
+  if (is_date(month)) months=months(month);
+  if (nchar(month)>3) which(MONTHS==month) else which(MONS==month);
+})
+match_month=Vectorize(function(month) {
+  if (is_date(month)) month=MONTHS(month) else month=ucfirst(month);
+  if (nchar(month)>3) match.arg(month,MONTHS) else match.arg(month,MONS);
+})
+## TODO: figure out how to vectorize: Vectorize turns dates into ints!
+inc_month=function(month,i=1) {
+  if (is_date(month)) return(seq(month,len=2,by=paste(i,'month'))[2]);
+  nmonth=((monthofyear(month)-1+i)%%12)+1;
+  if (nchar(month)>3) MONTHS[nmonth] else MONS[nmonth];
+}
 ## get date of Sunday for week containing date
 sunday_week=function(date) date-dayofweek(weekdays(date))+1
-
 ## adapted from www.r-bloggers.com/dates-in-r-and-the-first-day-of-the-month. Thx!
 ## NG 21-09-19: code moved here from covid/R/label.R
 ##              extended to get last day of month
